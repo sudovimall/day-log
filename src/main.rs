009e907cdb2e6ab017c116af9aa6d9d034388761
sudorm <1@t.com>
2026-02-09 22:12:54 +0800

    add: init

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| format!("{}=trace", env!("CARGO_CRATE_NAME")).into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+    
+}
