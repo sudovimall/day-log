e3b52f4c97d9e6f3cd97b05ab364d1f4abfd0b0f
sudorm <1@t.com>
2026-02-10 00:15:45 +0800

    feat: 分离server

---
+++ src/http/server.rs
@@ -0,0 +1,37 @@
+use axum::Router;
+use axum::routing::get;
+use log::debug;
+use tokio::net::TcpListener;
+use tracing::info;
+use crate::app_state::AppState;
+use crate::http::journal;
+
+pub async fn run(app_state: AppState) -> Result<(), Box<dyn std::error::Error>> {
+    let mut port = app_state.config.port;
+
+    let router
+        = Router::new()
+        .route("/journal", get(journal::create_journal))
+        .with_state(app_state);
+
+    loop {
+        match TcpListener::bind(format!("127.0.0.1:{}", port)).await {
+            Ok(listener) => {
+                info!("服务已启动 http://127.0.0.1:{}", port);
+                let _ = axum::serve(listener, router.into_make_service()).await?;
+                break;
+            },
+            Err(err) if err.kind() == std::io::ErrorKind::AddrInUse => {
+                debug!("端口 {} 被占用，尝试使用下一个端口", port);
+                port += 1;
+            },
+            Err(err) => {
+                return Err(Box::new(err));
+            }
+        }
+    }
+
+    Ok(())
+}
+
+
